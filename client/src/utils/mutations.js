c5b181f498130c3ae40a9bc6b15d6d0e983dcbba
Lauren A <laurenhaileyama@gmail.com>
2022-05-31 11:33:19 -0400

    fill out mutations file

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,65 @@ mutation login($email: String, $password: String!) {
     }
 }
 `;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _ud
+            username
+        }
+    }
+}
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook(
+    $userId: ID!
+    $authors: [String]
+    $description: String
+    $bookId: String
+    $image: String
+    $link: String
+    $title: String
+) {
+    saveBook(
+        userId: $userId
+        authors: $authors
+        description: $description
+        bookId: $bookId
+        image: $image
+        link: $link
+        title: $title
+    ) {
+        _id
+        username
+        savedBooks {
+            _id authors description
+            bookId
+            image
+            link
+            title
+        }
+    }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($userId: ID!, $bookId: ID!) {
+    removeBook(userId: $userId, bookId: $bookId) {
+        _id
+        username
+        savedBooks {
+            _id
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+    }
+}
+`;
